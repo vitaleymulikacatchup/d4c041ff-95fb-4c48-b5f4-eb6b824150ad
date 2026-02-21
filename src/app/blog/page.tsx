d3cb9e371012b158f00bc56b0171f23ff836a7d9
bender <kudin.dima@gmail.com>
2026-02-21 14:13:07 +0000

    Update src/app/blog/page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import ReactLenis from "lenis/react";
-import BlogCardTwo from '@/components/sections/blog/BlogCardTwo';
-import FooterCard from '@/components/sections/footer/FooterCard';
 import { ThemeProvider } from "@/providers/themeProvider/ThemeProvider";
 import NavbarStyleCentered from '@/components/navbar/NavbarStyleCentered/NavbarStyleCentered';
+import FooterCard from '@/components/sections/footer/FooterCard';
+import BlogCardOne from '@/components/sections/blog/BlogCardOne';
 import { Instagram, Twitter, Facebook } from 'lucide-react';
 import { useBlogPosts } from "@/hooks/useBlogPosts";
+
+const navItems = [
+    { name: "Home", id: "/" },
+    { name: "About", id: "/#about" },
+    { name: "Menu", id: "/#menu" },
+    { name: "Brew Process", id: "/#process" },
+    { name: "Contact", id: "/#contact" }
+];
 
 export default function BlogPage() {
     const { posts, isLoading } = useBlogPosts();
@@ -24,63 +31,53 @@ export default function BlogPage() {
             secondaryButtonStyle="glass"
             headingFontWeight="extrabold"
         >
-            <ReactLenis root>
-                <div id="nav" data-section="nav">
-                    <NavbarStyleCentered
-                        brandName="Bakteria"
-                        navItems={[
-                            { name: "Home", id: "/" },
-                            { name: "About", id: "/#about" },
-                            { name: "Menu", id: "/#menu" },
-                            { name: "Brew Process", id: "/#process" },
-                            { name: "Blog", id: "/blog" },
-                            { name: "Shop", id: "/shop" },
-                            { name: "Contact", id: "/#contact" }
-                        ]}
-                        button={{ text: "Order Now", href: "/#contact" }}
+            <div id="nav" data-section="nav">
+                <NavbarStyleCentered
+                    brandName="Bakteria"
+                    navItems={navItems}
+                    button={{ text: "Order Now", href: "/#contact" }}
+                />
+            </div>
+
+            {isLoading ? (
+                <div className="w-full mx-auto py-20 text-center">
+                    <p className="text-foreground">Loading posts...</p>
+                </div>
+            ) : (
+                <div id="blog" data-section="blog">
+                    <BlogCardOne
+                        blogs={posts}
+                        title="Coffee Stories & Insights"
+                        description="Discover the art of coffee roasting, brewing techniques, and the stories behind our exceptional beans"
+                        textboxLayout="default"
+                        useInvertedBackground={false}
+                        animationType="slide-up"
+                        carouselMode="buttons"
+                        tag="Blog"
                     />
                 </div>
+            )}
 
-                {isLoading ? (
-                    <div className="w-content-width mx-auto py-20 text-center">
-                        <p className="text-foreground">Loading posts...</p>
-                    </div>
-                ) : (
-                    <div id="blog" data-section="blog">
-                        <BlogCardTwo
-                            blogs={posts}
-                            title="Coffee Stories & Insights"
-                            description="Discover the art of coffee roasting, brewing techniques, and the stories behind our exceptional beans"
-                            textboxLayout="default"
-                            useInvertedBackground={false}
-                            animationType="slide-up"
-                            carouselMode="buttons"
-                            tag="Blog"
-                        />
-                    </div>
-                )}
-
-                <div id="footer" data-section="footer">
-                    <FooterCard
-                        logoText="Bakteria"
-                        copyrightText="© 2025 Bakteria Coffee Roastery. All rights reserved."
-                        socialLinks={[
-                            {
-                                icon: Instagram,
-                                href: "https://instagram.com/bakteriacoffee",                                ariaLabel: "Instagram"
-                            },
-                            {
-                                icon: Twitter,
-                                href: "https://twitter.com/bakteriacoffee",                                ariaLabel: "Twitter"
-                            },
-                            {
-                                icon: Facebook,
-                                href: "https://facebook.com/bakteriacoffee",                                ariaLabel: "Facebook"
-                            }
-                        ]}
-                    />
-                </div>
-            </ReactLenis>
+            <div id="footer" data-section="footer">
+                <FooterCard
+                    logoText="Bakteria"
+                    copyrightText="© 2025 Bakteria Coffee Roastery. All rights reserved."
+                    socialLinks={[
+                        {
+                            icon: Instagram,
+                            href: "https://instagram.com/bakteriacoffee",                            ariaLabel: "Instagram"
+                        },
+                        {
+                            icon: Twitter,
+                            href: "https://twitter.com/bakteriacoffee",                            ariaLabel: "Twitter"
+                        },
+                        {
+                            icon: Facebook,
+                            href: "https://facebook.com/bakteriacoffee",                            ariaLabel: "Facebook"
+                        }
+                    ]}
+                />
+            </div>
         </ThemeProvider>
     );
 }
